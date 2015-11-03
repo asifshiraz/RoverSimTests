9b5548ec9e6e273e69518c48a7ec49b63f0a4eaf
Muhammad Shiraz <asifshiraz@yahoo.com>
2015-11-03 02:32:54 -0600

    Updates

--- src/world/PseudoBooleanSolverTests.java
+++ src/world/PseudoBooleanSolverTests.java
@@ -56,7 +56,7 @@ public class PseudoBooleanSolverTests {
 		ArrayList<AgentScheduleQualities> input = gen.CreateRamdomScheduleQualities(i,j);
 		List<List<Integer>> taskCombinations = CreateCombinations(input);
 		
-		CalculatePlain(input, taskCombinations);
+		//CalculatePlain(input, taskCombinations);
 		
 		//int[] result1 = calc.SolveOptimizationProblem(input);
 		
@@ -97,16 +97,16 @@ public class PseudoBooleanSolverTests {
 		c.add(0);
 		
 		int[] assignments = new int[]{0,1,2};
-		Assert.assertEquals(true, IsAssignmentValid(assignments, agentsSize, taskCombinations) );
+		//Assert.assertEquals(true, IsAssignmentValid(assignments, agentsSize, taskCombinations) );
 		
-		b.add(2);Assert.assertEquals(false, IsAssignmentValid(assignments, agentsSize, taskCombinations) );
+		//b.add(2);Assert.assertEquals(false, IsAssignmentValid(assignments, agentsSize, taskCombinations) );
 	}
 	
 	@Test
 	public void BaseConversionTest()
 	{
 		int[] arr = null;
-		arr = ConvertBase10ToBaseNumberOfTasks(7,3,2);
+		/*arr = ConvertBase10ToBaseNumberOfTasks(7,3,2);
 		Assert.assertArrayEquals(ConvertBase10ToBaseNumberOfTasks(0,3,2), new int[]{0,0,0});
 		Assert.assertArrayEquals(ConvertBase10ToBaseNumberOfTasks(1,3,2), new int[]{0,0,1});
 		Assert.assertArrayEquals(ConvertBase10ToBaseNumberOfTasks(2,3,2), new int[]{0,1,0});
@@ -115,7 +115,7 @@ public class PseudoBooleanSolverTests {
 		Assert.assertArrayEquals(ConvertBase10ToBaseNumberOfTasks(5,3,2), new int[]{1,0,1});
 		Assert.assertArrayEquals(ConvertBase10ToBaseNumberOfTasks(6,3,2), new int[]{1,1,0});
 		Assert.assertArrayEquals(ConvertBase10ToBaseNumberOfTasks(7,3,2), new int[]{1,1,1});
-		Assert.assertArrayEquals(ConvertBase10ToBaseNumberOfTasks(8,3,2), new int[]{0,0,0});
+		Assert.assertArrayEquals(ConvertBase10ToBaseNumberOfTasks(8,3,2), new int[]{0,0,0});*/
 	}
 	
 	@Test
@@ -155,138 +155,6 @@ public class PseudoBooleanSolverTests {
 			}
 		}
 		return 0;
-	}
-	
-	private int[] ConvertBase10ToBaseNumberOfTasks(double number, int numAgents, int numTasksCombinations)
-	{
-		int[] result = new int[numAgents+1];
-		double remainder = number;
-		for(int i=numAgents; i>=0; i--)
-		{
-			double currentBase = Math.pow(numTasksCombinations,i);
-			result[numAgents-i] = (int)(remainder / currentBase);
-			remainder = remainder % currentBase;
-		}
-		return Arrays.copyOfRange(result, 1, result.length);
-	}
-	
-	private void CalculatePlain(ArrayList<AgentScheduleQualities> input, List<List<Integer>> taskCombinations)
-	{
-		MeasureTime.Timer2 = new MeasureTime();
-		MeasureTime.Timer2.Start();
-	
-		int taskCombinationsSize = taskCombinations.size();
-		
-		//Get all unique agents
-		List<Integer> agentIds = new ArrayList<Integer>();
-		for(int i=0;i<input.size();i++)
-		{
-			agentIds.add(input.get(i).AgentVariableId);
-		}
-		int agentsSize = agentIds.size();
-		
-		//Variable to hold all possible combination of tasks for any agent
-		Map<Integer[], Integer> agentTaskCombinationQualities = new HashMap<Integer[],Integer>();
-		
-		double totalPossibleValues = Math.pow(taskCombinationsSize, agentsSize);
-		//Populate all combinations
-		
-		int bestQuality = -9999999;
-		int[] bestCombination = new int[0];
-		
-		int[] lastValidAssignment = new int[agentsSize];
-		int[] nextAssignment = new int[agentsSize];
-		
-		for(int i=0; i<totalPossibleValues; i++)
-		{
-			nextAssignment = ConvertBase10ToBaseNumberOfTasks(i,agentsSize, taskCombinationsSize);
-			//System.out.println(Arrays.toString(assignments));
-			
-			if (!IsAssignmentValid(nextAssignment, agentsSize, taskCombinations))
-			{
-				Main.Message(debugFlag, RemapToAgentTasksFromTaskCombinations(nextAssignment, taskCombinations, input)+ " INVALID");
-				continue;
-			}
-			lastValidAssignment = nextAssignment;
-			//System.out.println(RemapAssignmentToAgentTasks(lastValidAssignment, taskCombinations));
-			int tempQuality = 0;
-			//Find combination quality
-			for(int agentIndex=0; agentIndex<agentsSize; agentIndex++)
-			{
-				int inn = lastValidAssignment[agentIndex];
-				try
-				{
-					List<Integer> tasksSet = taskCombinations.get( inn );
-					tempQuality += GetQualityIfAgentDoes( tasksSet, input.get(agentIndex)  );
-				}
-				catch(Exception ex)
-				{
-					System.out.println(inn + " " + ex);
-				}
-				
-			}
-			Main.Message(debugFlag, RemapToAgentTasksFromTaskCombinations(nextAssignment, taskCombinations, input) + " " + tempQuality);
-			//If this is best, assign
-			if (tempQuality>bestQuality)
-			{
-				bestQuality = tempQuality;
-				bestCombination = lastValidAssignment;
-			}
-		}
-		
-		MeasureTime.Timer2.Stop();
-		System.out.println("Plain Calculation Took " + MeasureTime.Timer2.GetTotal());
-		System.out.println(RemapToAgentTasksFromTaskCombinations(bestCombination, taskCombinations, input));
-	}
-	
-	private String RemapToAgentTasksFromTaskCombinations(int[] chosenAssignment, List<List<Integer>> taskCombinations, ArrayList<AgentScheduleQualities> input)
-	{
-		String r = "";
-		for(int i=0;i<chosenAssignment.length;i++)
-		{
-			r += input.get(i).AgentVariableId + " [";
-			List<Integer> tasks = taskCombinations.get(chosenAssignment[i]);
-			for(Integer in : tasks)
-			{
-				r += in + ",";
-			}
-			r += "] ";
-		}
-		return r;
-	}
-	
-	
-	
-	private boolean IsAssignmentValid(int[] assignments, int agentsSize, List<List<Integer>> taskCombinations)
-	{
-		boolean result = true;
-		List<Integer> tasksAlreadyTakenUpByAnAgent = new ArrayList<Integer>();
-		
-		//List<Integer> tasksSetPrevious = null;
-		for(int agentIndex=0; agentIndex<agentsSize; agentIndex++)
-		{
-			int inn = assignments[agentIndex];
-			try
-			{
-				List<Integer> tasksSet = taskCombinations.get( inn );
-				for(Integer task : tasksSet)
-				{
-					if (tasksAlreadyTakenUpByAnAgent.contains(task))
-					{
-						return false;
-					}
-					else
-					{
-						tasksAlreadyTakenUpByAnAgent.add(task);
-					}
-				}
-			}
-			catch(Exception ex)
-			{
-				System.out.println(inn + " " + ex);
-			}	
-		}
-		return result;
 	}
 	
 	
